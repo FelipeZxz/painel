b687ea5bbdca0cac9c7c293493e6b810ebe9b2e4
FelipeZxz <caubraudo10@gmail.com>
2025-08-23 21:49:02 -0300

    tt

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
-// Service Worker simples para comportamento PWA
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('ff-panel-v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/painel.html'
-            ]);
-        })
-    );
+const CACHE_NAME = 'ios-freefire-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
