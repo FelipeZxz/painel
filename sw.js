ad3a28c7a101a86de9ff3aa65036ff717d176fcf
FelipeZxz <caubraudo10@gmail.com>
2025-08-23 22:34:59 -0300

    asjd

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'ios-freefire-v1';
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
